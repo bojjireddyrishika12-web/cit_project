dca287aa59888dbcb6ac2071c1ff14eb60da175b
bojjireddyrishika12-web <bojjireddyrishika12@gmail.com>
2026-05-29 14:23:56 +0530

    project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,37 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+module tt_um_aishu_rng (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
 
-`default_nettype none
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire ena,
+    input  wire clk,
+    input  wire rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    reg [7:0] lfsr;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire feedback;
+
+    assign feedback = lfsr[7] ^ lfsr[5] ^ lfsr[4] ^ lfsr[3];
+
+    always @(posedge clk or negedge rst_n)
+    begin
+        if (!rst_n)
+            lfsr <= 8'hA5;
+
+        else if (uio_in[0])
+            lfsr <= ui_in;
+
+        else
+            lfsr <= {lfsr[6:0], feedback};
+    end
+
+    assign uo_out = lfsr;
+
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
