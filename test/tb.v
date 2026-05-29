ac2e258fdbbe6b25b615f73f932c8bbcbec4605b
bojjireddyrishika12-web <bojjireddyrishika12@gmail.com>
2026-05-29 16:05:12 +0530

    tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-    tt_um_pqc_aishu user_project (
+    tt_um_pqc_rishi user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
